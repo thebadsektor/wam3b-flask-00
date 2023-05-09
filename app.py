159846cec11b9ed288d4631b009e0cee6131338e
badsektor <gerald.villaran@lspu.edu.ph>
2023-05-09 15:44:53 +0800

    init

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<a href='/greet'>Hello Universe!</a>"
+
+@app.route('/greet')
+def greet():
+    return "<p>How are you?</p>"
